5e3c95115b3bab7555f05a1c41f7119c475cfaac
unknown <am01620@LOGTI-A3324-13.ens.ad.etsmtl.ca>
2016-02-05 12:48:10 -0500

    removed canceled exeption from simulation

--- src/main/java/edu/gordon/drivers/ICustomerConsole.java
+++ src/main/java/edu/gordon/drivers/ICustomerConsole.java
@@ -1,7 +1,6 @@
 package edu.gordon.drivers;
 
 import edu.gordon.banking.Money;
-import edu.gordon.simulation.drivers.CustomerConsole.Cancelled;
 
 public interface ICustomerConsole {
 	public void display(String message);
